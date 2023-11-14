6a2a4a1a81582108fc8097479d81fc416e848d21
Tyuiu.ShpilevayaAS@outlook.com <rina.ress@mail.ru>
2023-11-14 14:36:38 +0500

    Add project files.

---
+++ Tyuiu.ShpilevayaAS.Sprint4.TaskReview.V10.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ShpilevayaAS.Sprint4.TaskReview.V10.Lib;
+
+namespace Tyuiu.ShpilevayaAS.Sprint4.TaskReview.V10.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            int rows = 3;
+            int columns = 4;
+            int[,] mtrx = new int[rows, columns];
+            string str = "695847142536";
+            DataService ds = new DataService();
+            int res = ds.Calculate(rows, columns, str);
+            int wait = 30;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ShpilevayaAS.Sprint4.TaskReview.V10/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ShpilevayaAS.Sprint4.TaskReview.V10.Lib;
+
+namespace Tyuiu.ShpilevayaAS.Sprint4.TaskReview.V10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int rows = 3;
+            int columns = 4;
+            int[,] mtrx = new int[rows, columns];
+            string str = "695847142536";
+            int index = 0;
+
+            Console.Title = "Спринт #4 | Выполнила: Шпилевая А. С. | ИИПб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Sprint Review                                                     *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант 10                                                              *");
+            Console.WriteLine("* Выполнила: Шпилевая Арина Станиславовна | ИИПб-23-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дана строка из одноразрядных цифр '695847142536'.                       *");
+            Console.WriteLine("* Преобразуйте ее в матрицу 3 на 4 и подсчитайте сумму нечетных чисел.    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+            Console.WriteLine("\nМассив: ");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{str[index]}\t");
+                    index++;
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+
+            int res = ds.Calculate(rows, columns, str);
+            Console.WriteLine("Сумма нечетных чисел в матрице = " + res);
+
+            Console.ReadKey();
+        }
+    }
+}
